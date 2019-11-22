f4a1cde4a1839a5b4c4883e2a8c9aac2cdc7d890
noothanprem <noothanprem@gmail.com>
2019-11-22 11:59:13 +0530

    add : checks whether the entered last name is valid or not

--- userregistration.py
+++ userregistration.py
@@ -4,11 +4,21 @@ class UserRegistration:
 	
 	def first_name_check(self,first_name):
 		
-		result  = re.search("^[A-Z][a-z]{3,}",first_name)
+		pattern = "^[A-Z][a-z]{3,}"
+		result  = re.search(pattern,first_name)
 		if result:
-			print("Valid User name")
+			print("Valid First name")
 		else:
-			print("Invalid User name")
+			print("Invalid First name")
+
+	def last_name_check(self,last_name):
+		
+		pattern = "^[A-Z][a-z]{3,}"
+		result = re.search(pattern,last_name)
+		if result:
+			print("Valid Last name")
+		else:
+			print("Invalid Last name")
 
 			
 if __name__ == "__main__":
@@ -16,3 +26,5 @@ if __name__ == "__main__":
 	userregistration_object = UserRegistration()
 	first_name=input("Enter the first name : ")
 	userregistration_object.first_name_check(first_name)
+	last_name = input("Enter the last name : ")
+	userregistration_object.last_name_check(last_name)
