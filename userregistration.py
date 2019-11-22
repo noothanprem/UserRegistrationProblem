d0d3dfd4c14c9bc4839f077429285ec5ca243b09
noothanprem <noothanprem@gmail.com>
2019-11-22 19:08:10 +0530

    add : checks whether the email entered by the user is valid or not

--- userregistration.py
+++ userregistration.py
@@ -22,7 +22,7 @@ class UserRegistration:
 			print("Invalid Last name")
 
 	def email_check(self,email):
-		pattern = "^\w+([+\.-]?\w+)*@\w+([\.-]?\w{2,3})(\.\D{2,3})?$"
+		pattern = "^\w+([+\.-]?\w+)*@(\d?\w+)(\.\w{2,3})(\.\D{2,3})?$"
 		result = re.search(pattern,email)
 		if result:
 			print("Valid Email")
@@ -30,14 +30,27 @@ class UserRegistration:
 		else:
 			print("Invalid Email")
 
-			
+	def mobile_number_check(self,mobile_number):
+		pattern="[0-9]{2}[ ]?[0-9]{10}"
+		result = re.search(pattern,mobile_number)
+		if result:
+			print("Valid Mobile number")
+		else:
+			print("Invalid Mobile number")
+
+	
+
 if __name__ == "__main__":
 
 	userregistration_object = UserRegistration()
 	
-	first_name=input("Enter the first name : ")
-	userregistration_object.first_name_check(first_name)
-	last_name = input("Enter the last name : ")
-	userregistration_object.last_name_check(last_name)
+	#first_name=input("Enter the first name : ")
+	#userregistration_object.first_name_check(first_name)
+	#last_name = input("Enter the last name : ")
+	#userregistration_object.last_name_check(last_name)
 	email=input("Enter the email address : ")
 	userregistration_object.email_check(email)
+	#mobile_number = input("Enter the mobile number : ")
+	#userregistration_object.mobile_number_check(mobile_number)
+	#password = input("Enter the Password : ")
+	#userregistration_object.password_check(password)
