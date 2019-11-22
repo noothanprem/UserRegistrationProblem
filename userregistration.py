ac220cada4eac234e1450527e4b87652c95eb389
noothanprem <noothanprem@gmail.com>
2019-11-22 16:32:52 +0530

    add : check the email entered by the user for validation

--- userregistration.py
+++ userregistration.py
@@ -14,17 +14,30 @@ class UserRegistration:
 	def last_name_check(self,last_name):
 		
 		pattern = "^[A-Z][a-z]{3,}"
+		
 		result = re.search(pattern,last_name)
 		if result:
 			print("Valid Last name")
 		else:
 			print("Invalid Last name")
 
+	def email_check(self,email):
+		pattern = "^\w+([+\.-]?\w+)*@\w+([\.-]?\w{2,3})(\.\D{2,3})?$"
+		result = re.search(pattern,email)
+		if result:
+			print("Valid Email")
+
+		else:
+			print("Invalid Email")
+
 			
 if __name__ == "__main__":
 
 	userregistration_object = UserRegistration()
+	
 	first_name=input("Enter the first name : ")
 	userregistration_object.first_name_check(first_name)
 	last_name = input("Enter the last name : ")
 	userregistration_object.last_name_check(last_name)
+	email=input("Enter the email address : ")
+	userregistration_object.email_check(email)
